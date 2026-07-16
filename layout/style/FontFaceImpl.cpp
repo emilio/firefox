af293d605245792db5df37e7f0aeadf61716684b
Diego Escalante <descalante@mozilla.com>
2026-07-16 22:30:25 +0200

    Bug 1911075 - Rename font-stretch to font-width, and make the old property name an alias. r=jfkthame
    Differential Revision: https://phabricator.services.mozilla.com/D311312

--- layout/style/FontFaceImpl.cpp
+++ layout/style/FontFaceImpl.cpp
@@ -170,13 +170,13 @@ void FontFaceImpl::SetWeight(const nsACString& aValue, ErrorResult& aRv) {
   }
 }
 
-void FontFaceImpl::GetStretch(nsACString& aResult) {
-  GetDesc(FontFaceDescriptorId::FontStretch, aResult);
+void FontFaceImpl::GetWidth(nsACString& aResult) {
+  GetDesc(FontFaceDescriptorId::FontWidth, aResult);
 }
 
-void FontFaceImpl::SetStretch(const nsACString& aValue, ErrorResult& aRv) {
+void FontFaceImpl::SetWidth(const nsACString& aValue, ErrorResult& aRv) {
   mFontFaceSet->FlushUserFontSet();
-  if (SetDescriptor(FontFaceDescriptorId::FontStretch, aValue, aRv)) {
+  if (SetDescriptor(FontFaceDescriptorId::FontWidth, aValue, aRv)) {
     DescriptorUpdated();
   }
 }
@@ -501,12 +501,18 @@ bool FontFaceImpl::SetDescriptors(const nsACString& aFamily,
     return false;
   };
 
+  nsCString widthOrStretch;
+  if (aDescriptors.mWidth.WasPassed()) {
+    widthOrStretch = aDescriptors.mWidth.Value();
+  } else if (aDescriptors.mStretch.WasPassed()) {
+    widthOrStretch = aDescriptors.mStretch.Value();
+  }
   // Parse all of the mDescriptors in aInitializer, which are the values
   // we got from the JS constructor.
   if (!setDesc(FontFaceDescriptorId::FontFamily, aFamily) ||
       !setDesc(FontFaceDescriptorId::FontStyle, aDescriptors.mStyle) ||
       !setDesc(FontFaceDescriptorId::FontWeight, aDescriptors.mWeight) ||
-      !setDesc(FontFaceDescriptorId::FontStretch, aDescriptors.mStretch) ||
+      !setDesc(FontFaceDescriptorId::FontWidth, widthOrStretch) ||
       !setDesc(FontFaceDescriptorId::UnicodeRange,
                aDescriptors.mUnicodeRange) ||
       !setDesc(FontFaceDescriptorId::FontFeatureSettings,
@@ -651,10 +657,10 @@ bool FontFaceImpl::GetAttributesFromRule(
     aAttr.mWeight = WeightRange(weightRange._0, weightRange._1);
   }
 
-  StyleComputedFontStretchRange stretchRange;
-  if (Servo_FontFaceRule_GetFontStretch(aData, &stretchRange)) {
-    aAttr.mRangeFlags &= ~gfxFontEntry::RangeFlags::eAutoStretch;
-    aAttr.mStretch = StretchRange(stretchRange._0, stretchRange._1);
+  StyleComputedFontWidthRange widthRange;
+  if (Servo_FontFaceRule_GetFontWidth(aData, &widthRange)) {
+    aAttr.mRangeFlags &= ~gfxFontEntry::RangeFlags::eAutoWidth;
+    aAttr.mWidth = WidthRange(widthRange._0, widthRange._1);
   }
 
   StyleComputedFontStyleRange styleRange;
